61b86e8fcc7c7a848ef69d96e478a56a32d0793b
sonhoang1809 <sonhoang1809@gmail.com>
2020-05-15 23:37:05 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-demacia
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-demacia)
---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css', './css/bootstrap.min.css','./css/style.css' ]
+})
+export class AppComponent  {
+  name = 'Angular';
+}
