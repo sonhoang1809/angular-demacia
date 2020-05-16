a2682389ea039f1c2cfef5fac82f952c98ca75c9
sonhoang1809 <sonhoang1809@gmail.com>
2020-05-16 16:42:48 +0700

    4 files modified, 1 added

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css', '../css/bootstrap.min.css','../css/style.css']
 })
 export class HeaderComponent implements OnInit {
 
