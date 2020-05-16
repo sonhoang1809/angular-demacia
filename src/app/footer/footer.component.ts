5685d4b5dc1e6d07d5959f33c023b423fea95031
sonhoang1809 <sonhoang1809@gmail.com>
2020-05-16 16:19:38 +0700

    5 files modified, 2 added

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -3,8 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css', '../css/bootstrap.min.css','../css/style.css']
 })
+
 export class FooterComponent implements OnInit {
 
   constructor() { }
