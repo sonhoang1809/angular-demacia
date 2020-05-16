c462327fffa3bd2eca8ed4c594dd80fcfea55427
sonhoang1809 <sonhoang1809@gmail.com>
2020-05-16 23:14:31 +0700

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HeaderEndComponent } from './header-end/header-end.component';
