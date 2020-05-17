551e06e25a872c015fcab34d42f27d66caf48760
sonhoang1809 <sonhoang1809@gmail.com>
2020-05-17 21:49:03 +0700

    2 files modified

--- src/app/header/menu-bar/menu-bar.component.ts
+++ src/app/header/menu-bar/menu-bar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Image } from '../../image';
 
 @Component({
   selector: 'app-menu-bar',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuBarComponent implements OnInit {
 
-  constructor() { }
+  adidasMenuImg: Image;
+  nikeMenuImg: Image;
+  pumaMenuImg: Image;
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.http.get<Image>('https://demacia-shop.azurewebsites.net/api/Images/ImgsMenuAdidas').subscribe(result =>{
+      this.adidasMenuImg=result;
+      this.adidasMenuImg.imageData = 'data:image/jpg;base64,'+this.adidasMenuImg.imageData;
+    });
+    this.http.get<Image>('https://demacia-shop.azurewebsites.net/api/Images/ImgsMenuNike').subscribe(result =>{
+      this.nikeMenuImg=result;
+      this.nikeMenuImg.imageData = 'data:image/jpg;base64,'+this.nikeMenuImg.imageData;
+    });
+    //this.http.get<Image>('https://localhost:44326/api/Images/ImgsMenuPuma').subscribe(result =>{
+    //  this.nikeMenuImg=result;
+   // });
   }
 
 }
