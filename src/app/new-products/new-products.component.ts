743993d7772be7d161cb19b87f7924f1316e1fca
sonhoang1809 <sonhoang1809@gmail.com>
2020-05-17 12:36:57 +0700

    2 files modified

--- src/app/new-products/new-products.component.ts
+++ src/app/new-products/new-products.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Image } from "../image";
+import { HttpClient } from "@angular/common/http";
+//import 'rxjs/add/operator/map'
+//import { Http , Response} from "@angular/http";
 
 @Component({
-  selector: 'app-new-products',
-  templateUrl: './new-products.component.html',
-  styleUrls: ['./new-products.component.css']
+  selector: "app-new-products",
+  templateUrl: "./new-products.component.html",
+  styleUrls: ["./new-products.component.css"]
 })
 export class NewProductsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  topImg: Image[];
+  img: any = {};
+  apiLink: "https://demacia-shop.azurewebsites.net/api/news";
+  constructor(private http: HttpClient) {
+    this.http.get<Image[]>(this.apiLink).subscribe(result => {
+      this.topImg = result;
+    });
+    //console.log(this.topImg);
+    //this.getImages();
   }
 
+  // getData() {
+  //   return this.http
+  //     .get(this.apiLink)
+  //     .map((response: Response) => response.json);
+  // }
+  // getImages() {
+  //   this.getData().subscribe(result => {
+  //     console.log(result);
+  //     this.img = result;
+  //   });
+  // }
+
+  ngOnInit() {}
 }
