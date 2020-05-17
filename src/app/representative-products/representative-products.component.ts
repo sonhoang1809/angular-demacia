9d5f405f3155b8bd465d0d1bec4b57a20c8adff1
sonhoang1809 <sonhoang1809@gmail.com>
2020-05-17 12:34:18 +0700

    1 file modified

--- src/app/representative-products/representative-products.component.ts
+++ src/app/representative-products/representative-products.component.ts
@@ -1,33 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Image } from '../image';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Image } from "../image";
 
 @Component({
-  selector: 'app-representative-products',
-  templateUrl: './representative-products.component.html',
-  styleUrls: ['./representative-products.component.css']
+  selector: "app-representative-products",
+  templateUrl: "./representative-products.component.html",
+  styleUrls: ["./representative-products.component.css"]
 })
 export class RepresentativeProductsComponent implements OnInit {
-
   imgAdidas: Image;
   imgNike: Image;
 
   constructor(private http: HttpClient) {
-    this.http.get<Image>("https://localhost:44326/api/Images/7").subscribe(
-      result =>{
+    this.http
+      .get<Image>("https://demacia-shop.azurewebsites.net/api/Images/7")
+      .subscribe(result => {
         this.imgAdidas = result;
-        this.imgAdidas.imageData = 'data:image/jpg;base64,'+this.imgAdidas.imageData;
-      }
-    );
-    this.http.get<Image>("https://localhost:44326/api/Images/405").subscribe(
-      result =>{
+        this.imgAdidas.imageData =
+          "data:image/jpg;base64," + this.imgAdidas.imageData;
+      });
+    this.http
+      .get<Image>("https://demacia-shop.azurewebsites.net/api/Images/405")
+      .subscribe(result => {
         this.imgNike = result;
-        this.imgNike.imageData = 'data:image/jpg;base64,'+this.imgNike.imageData;
-      }
-    );
-   }
-
-  ngOnInit() {
+        this.imgNike.imageData =
+          "data:image/jpg;base64," + this.imgNike.imageData;
+      });
   }
 
+  ngOnInit() {}
 }
